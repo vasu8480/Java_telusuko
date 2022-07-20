8c1444fc540d3baba2222f5b283c7bc9c7de093e
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-07-20 11:58:56 +0530

    Update and rename 7 Array.java to 7 Array_Enhanced_for_loop.java

--- 7 Array_Enhanced_for_loop.java
+++ 7 Array_Enhanced_for_loop.java
@@ -24,5 +24,22 @@ public class Array {
 			System.out.println();
 		}
 	}
+}	
 
+public class Enhanced_for_loop {
+static int i;
+public static void main(String[] args) {
+int a[]={1,3,4,7};
+
+		//print all elements in array		
+//		for(i=0;i<4;i++)
+//		{
+//			System.out.println(a[i]);
+//		}
+			//Enhanced For Loop	
+		for(int j: a)
+		{
+			System.out.print(" "+j);
+		}
+	}
 }
