37558735b5c70bffb423bbf91f471493e461aded
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-07-20 12:05:59 +0530

    Add files via upload

---
+++ 9_15 Abstract.java
@@ -0,0 +1,23 @@
+abstract class Human
+{
+	public abstract void eat();
+	public void walk()
+	{
+		
+	}
+}
+class Man extends Human
+{
+	public void eat()
+	{
+		
+	}
+}
+public class Abstract {
+
+	public static void main(String[] args) {
+		Human obj =new Man();
+
+	}
+
+}
---
+++ 9_16 Abstract_Number.java
@@ -0,0 +1,17 @@
+class print
+{
+	public void show(Number i)
+	{
+		System.out.println(i);
+	}
+}
+public class Abstract_Number {
+
+	public static void main(String[] args) {
+	
+		print obj=new print();
+		obj.show(54.255);
+
+	}
+
+}
