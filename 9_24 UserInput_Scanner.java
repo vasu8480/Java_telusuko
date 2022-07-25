79e464533970bd30f23faf34c6869124bf077a9b
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-07-25 10:50:47 +0530

    Add files via upload

---
+++ 9_24 UserInput_Scanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UserInput_Scanner {
+
+	public static void main(String[] args) {
+			int n;
+			System.out.println("enter a number");
+			Scanner sc=new Scanner(System.in);
+			n=sc.nextInt();
+		System.out.println(n);
+			
+	}
+}
